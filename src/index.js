58c6f88faac2faf9b0082e58023bdc1135de2bc3
vanbaotran <vanbaotran201194@gmail.com>
2021-10-10 20:25:07 +0200

    actions created

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import store from './store';
-
+import { bugAdded, bugResolved } from './actions';
 const unsubscribe = store.subscribe(()=>{
     console.log('Store changed!', store.getState());
 })
 
-store.dispatch({
-    type:"bugAdded",
-    payload:{
-        description:"Bug1"
-    }
-})
-unsubscribe();
+store.dispatch(bugAdded('Bug 1'));
 
-store.dispatch({
-    type:"budRemoved",
-    payload:{
-        id: 1,
-    }
-})
+// store.dispatch({
+//     type:actions.BUG_REMOVED,
+//     payload:{
+//         id: 1,
+//     }
+// })
+store.dispatch(bugResolved(1))
 console.log(store)
--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,9 @@
+import * as actions from './actionTypes'
 let lastId = 0;
 
 export default function reducer (state = [], action) {
     switch (action.type) {
-        case "bugAdded":
+        case actions.BUG_ADDED:
           return [
             ...state,
             {   
@@ -12,8 +13,12 @@ export default function reducer (state = [], action) {
                 },
             
         ]
-        case "budRemoved":
+        case actions.BUG_REMOVED:
             return state.filter(bug => bug.id !== action.payload.id)
+        case actions.BUG_RESOLVED:
+            return state.map(bug => 
+            bug.id !== action.payload.id ? bug : {...bug,resolved: true}
+            )
         default: 
             return state;
     }
